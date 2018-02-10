9936bf4345261983b382442b367382f395f94bf2
william valencia <williamvalencia@seven4n.com>
2018-02-10 12:05:41 -0500

    cambios clase

--- src/test/scala/ExampleTest/ExampleTest.scala
+++ src/test/scala/ExampleTest/ExampleTest.scala
@@ -15,11 +15,60 @@ class ExampleTest extends FunSuite {
     assert(home.equalsIgnoreCase("home2"));
   }
 
+  test("factorial"){
+    def simularWhile[A](valorMientras:Boolean ):Any = {
+      if(valorMientras) {
+        print(5*5)
+        simularWhile(true)
+      }
+    }
+  }
+
+  test("evaluar if-else"){
+
+    def ifELse(a:Boolean, x:Boolean, hh:Any, ba:Any, ab:Any) = {
+      if(a) ba else if (x) ab else hh
+    }
+  }
+
+  test("imprimir valor del factorial"){
+
+    def calcularFacturial(a:Int): Int = ???
+
+    def imprimirValor(factorial:Int) ={
+      s"El factorial del número es: $factorial"
+    }
+
+    //Composición de funciones
+    val x = (calcularFacturial _ andThen imprimirValor )(3)
+
+    val y = (imprimirValor _ compose calcularFacturial _ )(3)
+
+    def foo(a:Int, b:Int) = ???
+
+    def hof[A,  B](x: A, msg: String, f: A => B) = {
+      s"el $msg es : ${f(x)}"
+    }
+
+    def dividir [A <: Double](dividendo:A, divisor:A) = {
+      if(!divisor.eq(0)) dividendo/divisor
+    }
+
+    def div:Double => Double => Double = x => y match y {
+      case y != 0 => x/y
+    }
+
+  }
+
+  test("uso of and then"){
+
+  }
+
   test("Test functions") {
 
     def writePretty(a:Int) = "Esto es un: " + a
 
-    def ordenizePreviewPrint(x:String, a:Int) = writePretty(a) + x
+    def ordenizePreviewPrint(x:String, a:Int): String = writePretty(a) + x
 
     def findArea(l:Double) = l*4
     val areaOfFour = findArea(4)
