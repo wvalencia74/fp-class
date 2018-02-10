c3062f802e6c30b7d470f5f27e00b6fd1738e967
William Valencia <williamvalencia@seven4n.com>
2018-02-09 22:44:46 -0500

    Some test about syntax

--- src/test/scala/ExampleTest/ExampleTest.scala
+++ src/test/scala/ExampleTest/ExampleTest.scala
@@ -9,8 +9,25 @@ class ExampleTest extends FunSuite {
 
   test("Define a simple val") {
 
-    val home = "home"
-    assert(home.equalsIgnoreCase("home"));
+    val home = "home" + 2
+
+    assert(home.size.equals(5))
+    assert(home.equalsIgnoreCase("home2"));
+  }
+
+  test("Test functions") {
+
+    def writePretty(a:Int) = "Esto es un: " + a
+
+    def ordenizePreviewPrint(x:String, a:Int) = writePretty(a) + x
+
+    def findArea(l:Double) = l*4
+    val areaOfFour = findArea(4)
+
+    assert(!writePretty(3).isEmpty)
+    assert(ordenizePreviewPrint(" gracias!", 3).equalsIgnoreCase("Esto es un: 3 gracias!"))
+    assert(areaOfFour.equals(16.0))
+
   }
 
 }
